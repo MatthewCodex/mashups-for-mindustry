2ea9796873615e3ff6a0c07c450159ee31f3d9fe
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-08-02 08:15:21 -0400

    Implement random ambient track playback with delay
    Added functionality to play a random ambient/dark track after a delay of 3.5 minutes.

--- scripts/main.js
+++ scripts/main.js
@@ -18,3 +18,43 @@ Events.on(ClientLoadEvent, active => {
     Vars.control.sound.darkMusic = Seq.with(Musics.game9, mus("game10"), mus("game11"), mus("game12"), mus("game13"), mus("game14"), mus("game15"));
     Vars.control.sound.bossMusic = Seq.with(Musics.game1, Musics.game2, Musics.game3, Musics.game4, Musics.game5, Musics.game6, Musics.game7, Musics.game8, Musics.game9);
 });
+
+// 3.5 minutes converted to seconds (3.5 * 60 = 210 seconds)
+const delayInSeconds = 210; 
+
+// List of vanilla ambient/dark game track objects
+const specialTracks = [
+    Musics.game1, // Originally called "Ambient"
+    Musics.game2,
+    Musics.game3,
+    Musics.game4,
+    Musics.game5,
+    Musics.game6,
+    Musics.game7,
+    Musics.game8,
+    Musics.game9,
+    mus("game10"),
+    mus("game11"),
+    mus("game12"),
+    mus("game13"),
+    mus("game14"),
+    mus("game15"),
+
+];
+
+// Pick a random track from the array
+const randomTrack = specialTracks[Math.floor(Math.random() * specialTracks.length)];
+
+// Schedule the song to play after the target delay
+Timer.schedule(() => {
+    try {
+        Vars.control.sound.stopMusic();
+
+        Vars.control.sound.playMusic(randomTrack);
+        Log.info("Playing selected ambient/dark track: " + randomTrack);
+    } catch(err) {
+        Log.err("Failed to play track: " + err);
+    }
+}, delayInSeconds);
+
+Log.info("Timer started. Ambient track will play in 3.5 minutes.");
