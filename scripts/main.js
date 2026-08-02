59494f09ffa127a636ce8ab64daa32174a9d6b95
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-08-01 20:31:43 -0400

    Refactor music handling on client load event

--- scripts/main.js
+++ scripts/main.js
@@ -8,41 +8,13 @@ mod.meta.description = Core.bundle.get("mod.description");
 function mus(e) {
   return Vars.tree.loadMusic(e);
 }
-var newcontent = Vars.mods.locateMod("newunits") != null;
-if (typeof Vars.content.planet("newunits-clexon") === "undefined") {
-  var clexon = null;
-} else {
-  var clexon = Vars.content.planet("newunits-clexon");
-}
-function clearMusic() {
-  Vars.control.sound.ambientMusic = Seq.with();
-  Vars.control.sound.darkMusic = Seq.with();
-  Vars.control.sound.bossMusic = Seq.with();
-};
-function callOnPlanetChange(pl) {
-  switch(pl) {
-    case (Planets.serpulo):
-          
-        Vars.control.sound.ambientMusic = Seq.with(Musics.game1, Musics.game2, Musics.game3, Musics.game4, Musics.game5, Musics.game6, Musics.game7, Musics.game8, Musics.fine);
-        Vars.control.sound.darkMusic = Seq.with(Musics.game9, mus("game10"), mus("game11"), mus("game12"), mus("game13"), mus("game14"), mus("game15"));
-        Vars.control.sound.bossMusic = Seq.with(Musics.boss1, Musics.boss2, Musics.game2);
-      
-    case (Planets.sun): //So apparently the "<Any>" option in Mindustry sets the planet to the sun... wonderful.
-    case (Planets.erekir):
-          
-        Vars.control.sound.ambientMusic = Seq.with(Musics.game1, Musics.game2, Musics.game3, Musics.game4, Musics.game5, Musics.game6, Musics.game7, Musics.game8, Musics.fine);
-        Vars.control.sound.darkMusic = Seq.with(Musics.game9, mus("game10"), mus("game11"), mus("game12"), mus("game13"), mus("game14"), mus("game15"));
-        Vars.control.sound.bossMusic = Seq.with(Musics.boss1, Musics.boss2, Musics.game2);
+
         
-        
-        return;
-      
-  };
-};
-let planet = null, planetCur = null;
-Events.run(Trigger.update, () => {
-  if(Vars.state.isMenu() || Vars.state.isEditor()) return;
-  planetCur = Vars.state.getPlanet();
-  if(planetCur != planet) callOnPlanetChange(planetCur);
-  planet = planetCur;
+
+Events.on(ClientLoadEvent, active => {
+    Log.info("Mindustry Client has started up!");
+    // Your main-menu or global initialization logic goes here
+    Vars.control.sound.ambientMusic = Seq.with(Musics.game1, Musics.game2, Musics.game3, Musics.game4, Musics.game5, Musics.game6, Musics.game7, Musics.game8, Musics.fine);
+    Vars.control.sound.darkMusic = Seq.with(Musics.game9, mus("game10"), mus("game11"), mus("game12"), mus("game13"), mus("game14"), mus("game15"));
+    Vars.control.sound.bossMusic = Seq.with(Musics.boss1, Musics.boss2, Musics.game2);
 });
