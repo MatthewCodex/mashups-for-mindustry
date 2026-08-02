fbb699af77bdb8149df9dda0df67e3081338ac06
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-08-02 08:09:18 -0400

    Modify music sequences for ambient and boss tracks
    Updated ambient and boss music sequences in the main script.

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ function mus(e) {
 Events.on(ClientLoadEvent, active => {
     Log.info("Mindustry Client has started up!");
     // Your main-menu or global initialization logic goes here
-    Vars.control.sound.ambientMusic = Seq.with(Musics.game1, Musics.game2, Musics.game3, Musics.game4, Musics.game5, Musics.game6, Musics.game7, Musics.game8, Musics.fine);
+    Vars.control.sound.ambientMusic = Seq.with(Musics.game1, Musics.game2, Musics.game3, Musics.game4, Musics.game5, Musics.game6, Musics.game7, Musics.game8);
     Vars.control.sound.darkMusic = Seq.with(Musics.game9, mus("game10"), mus("game11"), mus("game12"), mus("game13"), mus("game14"), mus("game15"));
-    Vars.control.sound.bossMusic = Seq.with(Musics.boss1, Musics.boss2, Musics.game2);
+    Vars.control.sound.bossMusic = Seq.with(Musics.game1, Musics.game2, Musics.game3, Musics.game4, Musics.game5, Musics.game6, Musics.game7, Musics.game8, Musics.game9);
 });
